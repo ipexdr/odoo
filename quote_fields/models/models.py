842f4c877f4b81a66af1a517a559cde37968dc7f
pdeoleo <pdeoleo@ipexdr.com>
2020-03-03 14:53:54 -0400

    list_price = compute _get_list_price

--- quote_fields/models/models.py
+++ quote_fields/models/models.py
@@ -2,21 +2,18 @@
 
 from odoo import models, fields, api
 
+
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
 
     # list_price = fields.Float('List Price', readonly=True, digits='Product Price', store=True)
-    list_price = fields.Float('List Price')
+    list_price = fields.Float('List Price', compute='_get_list_price')
     vendor_discount = fields.Float('Vendor Discount', widget='percentage')
-
 
     @api.depends('product_id')
     def _get_list_price(self):
         for record in self:
             record['list_price'] = record.product_id.standard_price
-
-
-
 
 # class quote_fields(models.Model):
 #     _name = 'quote_fields.quote_fields'
