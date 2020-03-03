5f2ec69526d857ad96821c27b5e9874f06e6d406
pdeoleo <pdeoleo@ipexdr.com>
2020-03-03 10:22:03 -0400

    Set list_price - view and model

--- quote_fields/models/sale_order.py
+++ quote_fields/models/sale_order.py
@@ -2,7 +2,20 @@
 
 from odoo import models, fields, api
 
-class SaleOrderInherit(models.Model):
-    _inherit = 'sale.order'
 
-# class SaleOrderLineInherit(models.Model)
+# class SaleOrderInherit(models.Model):
+#     _inherit = 'sale.order'
+
+
+class SaleOrderLineInherit(models.Model):
+    _inherit = 'sale.order.line'
+
+    @api.depends('product_id')
+    def _get_list_price(self):
+        for record in self:
+            record['list_price'] = record.product_id.standard_price
+
+    list_price = fields.Float('List Price', readonly=True, digits='Product Price', store=True)
+
+
+
