40797d6355403749dd8a33f1b7221ba3b7c62b7c
pdeoleo <pdeoleo@ipexdr.com>
2020-03-03 11:48:43 -0400

    models to models.py | views to views.xml

--- quote_fields/models/models.py
+++ quote_fields/models/models.py
@@ -1,6 +1,19 @@
 # -*- coding: utf-8 -*-
 
-# from odoo import models, fields, api
+from odoo import models, fields, api
+
+class SaleOrderLine(models.Model):
+    _inherit = 'sale.order.line'
+
+    # list_price = fields.Float('List Price', readonly=True, digits='Product Price', store=True)
+    list_price = fields.Char('List Price')
+
+    # @api.depends('product_id')
+    # def _get_list_price(self):
+    #     for record in self:
+    #         record.list_price = record.product_id.standard_price
+
+
 
 
 # class quote_fields(models.Model):
