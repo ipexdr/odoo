8dda4ecc84a824cc92a21a2c8cc1da292aeb4bb6
pdeoleo <pdeoleo@ipexdr.com>
2020-03-03 14:44:59 -0400

    list_price - Char to Float

--- quote_fields/models/models.py
+++ quote_fields/models/models.py
@@ -6,12 +6,13 @@ class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
 
     # list_price = fields.Float('List Price', readonly=True, digits='Product Price', store=True)
-    list_price = fields.Char('List Price')
+    list_price = fields.Float('List Price')
 
-    # @api.depends('product_id')
-    # def _get_list_price(self):
-    #     for record in self:
-    #         record.list_price = record.product_id.standard_price
+
+    @api.depends('product_id')
+    def _get_list_price(self):
+        for record in self:
+            record['list_price'] = record.product_id.standard_price
 
 
 
