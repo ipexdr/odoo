657162253f4e1e2398ebc55ea6eb9928c1351fc2
pdeoleo <pdeoleo@ipexdr.com>
2020-03-03 09:00:00 -0400

    Manifest correction - depends

--- quote_fields/__manifest__.py
+++ quote_fields/__manifest__.py
@@ -34,7 +34,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'sale_management'],
+    'depends': ['sale_management'],
 
     # always loaded
     'data': [
