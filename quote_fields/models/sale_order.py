7a4a7969201abe38ae45d5a0d4b16bf5bc182817
pdeoleo <pdeoleo@ipexdr.com>
2020-03-03 09:10:18 -0400

    sale_order inherited

--- quote_fields/models/sale_order.py
+++ quote_fields/models/sale_order.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
 
-# from odoo import models
-#
-# class
+from odoo import models, fields, api
+
+class SaleOrderInherit(models.Model):
+    _inherit = 'sale.order'
+
+# class SaleOrderLineInherit(models.Model)
