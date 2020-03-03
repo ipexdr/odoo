4cd857957d6ed376b783254b8d2c36174d4e78e3
pdeoleo <pdeoleo@ipexdr.com>
2020-03-03 11:37:16 -0400

    list_price - view and model - commented _get_list_price

--- quote_fields/models/sale_order.py
+++ quote_fields/models/sale_order.py
@@ -10,12 +10,12 @@ from odoo import models, fields, api
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
 
-    @api.depends('product_id')
-    def _get_list_price(self):
-        for record in self:
-            record.list_price = record.product_id.standard_price
+    # list_price = fields.Float('List Price', readonly=True, digits='Product Price', store=True)
+    list_price = fields.Char('List Price')
 
-    list_price = fields.Float('List Price', readonly=True, digits='Product Price', store=True)
-
+    # @api.depends('product_id')
+    # def _get_list_price(self):
+    #     for record in self:
+    #         record.list_price = record.product_id.standard_price
 
 
