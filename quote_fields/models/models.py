63cbf23ab0f7d3d45d021472d78b91e964e548d1
pdeoleo <pdeoleo@ipexdr.com>
2020-03-03 15:44:04 -0400

    add all fields to models and view

--- quote_fields/models/models.py
+++ quote_fields/models/models.py
@@ -9,7 +9,20 @@ class SaleOrderLine(models.Model):
     # list_price = fields.Float('List Price', readonly=True, digits='Product Price', store=True)
     list_price = fields.Float('List Price', compute='_get_list_price', readonly=True, store=True)
     vendor_discount = fields.Float('Vendor Discount', store=True)
-    vendor_discounted = fields.Float('Discounted', store=True)
+    vendor_discounted = fields.Float('Discounted', store=True, readonly=True)
+    # New
+    fob_total = fields.Float('FOB Total', store=True, readonly=True)  # _get_fob_total
+    tariff = fields.Float('Tariff', store=True)
+    tariff_cost = fields.Float('Tariff Cost', store=True, readonly=True)  # _get_tariff_cost
+    total_tariff_cost = fields.Float('Total Tariff Cost', store=True, readonly=True)  # _get_total_tariff_cost
+    cost = fields.Float('Cost', store=True, readonly=True)  # _get_cost
+    admin_cost = fields.Float('Admin. Cost', store=True)
+    final_cost = fields.Float('Final Cost', store=True, readonly=True)  # _get_final_cost
+    total_final_cost = fields.Float('Total Final Cost', store=True, readonly=True)  # _get_total_final_cost
+    margin = fields.Float('Margin', store=True)
+    profit_margin = fields.Float('Profit Margin', store=True, readonly=True)  # _get_profit_margin
+    profit = fields.Float('Profit', store=True, readonly=True)  # _get_profit
+    sell_price = fields.Float('Sell Price', store=True, readonly=True)  # _get_sell_price
 
     @api.depends('product_id')
     def _get_list_price(self):
