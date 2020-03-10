6c3da2d91c28fa3b4e22b2d01b46f2a6a9288603
pdeoleo <pdeoleo@ipexdr.com>
2020-03-10 11:49:30 -0400

    removed 'hr' from 'depends' in manifest

--- quote_fields/__manifest__.py
+++ quote_fields/__manifest__.py
@@ -34,7 +34,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['sale_management', 'hr'],
+    'depends': ['sale_management'],
 
     # always loaded
     'data': [
