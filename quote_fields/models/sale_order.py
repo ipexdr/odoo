041cc0df830fd81adba81344360d387a13807556
pdeoleo <pdeoleo@ipexdr.com>
2020-03-16 16:06:15 -0400

    removed extra quotation_approved state

--- quote_fields/models/sale_order.py
+++ quote_fields/models/sale_order.py
@@ -5,18 +5,8 @@ from odoo import models, fields, api
 
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
-
-    state = fields.Selection([
-        ('draft', 'Quotation'),
-        ('quotation_approved', "Quotation Approved"),
-        ('sent', 'Quotation Sent'),
-        ('sale', 'Sales Order'),
-        ('done', 'Locked'),
-        ('cancel', 'Cancelled'),
-    ], string='Status', readonly=True, copy=False, index=True, tracking=3, default='draft')
-
-    #     state = fields.Selection(selection_add_after={'draft': [('quotation_approved', 'Quotation Approved')]})
+    quote_approved = fields.Boolean(store=True, string="Is approved", default=False)
 
     def action_quotation_approve(self):
         for quote in self:
-            quote.state = 'quotation_approved'
+            quote.approved = True
