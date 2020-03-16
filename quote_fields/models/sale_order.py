25250fb9a1d5d2feac203acf61149e688b427004
pdeoleo <pdeoleo@ipexdr.com>
2020-03-16 09:38:07 -0400

    sale_order.py correction

--- quote_fields/models/sale_order.py
+++ quote_fields/models/sale_order.py
@@ -6,7 +6,16 @@ from odoo import models, fields, api
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
-    state = fields.Selection(selection_add=[('quotation_approved', "Quotation Approved")])
+    state = fields.Selection([
+        ('draft', 'Quotation'),
+        ('quotation_approved', "Quotation Approved"),
+        ('sent', 'Quotation Sent'),
+        ('sale', 'Sales Order'),
+        ('done', 'Locked'),
+        ('cancel', 'Cancelled'),
+    ], string='Status', readonly=True, copy=False, index=True, tracking=3, default='draft')
+
+    #     state = fields.Selection(selection_add_after={'draft': [('quotation_approved', 'Quotation Approved')]})
 
     def action_quotation_approve(self):
         for quote in self:
