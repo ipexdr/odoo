2dae8adc085be1ac20b502b44b673fd7b67018b4
pdeoleo <pdeoleo@ipexdr.com>
2020-03-17 10:57:51 -0400

    added approved_by_discount function

--- quote_fields/models/sale_order.py
+++ quote_fields/models/sale_order.py
@@ -1,12 +1,26 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
-
+import logging
 
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
-    quote_approved = fields.Boolean(store=True, string="Is approved", default=False)
+    quote_approved = fields.Boolean(store=True, string="Is approved", default=True)
+    pre_appr_disc = 5
+    approved_disc = fields.Float(store=True, string="Approved Discount", default=5)
 
     def action_quotation_approve(self):
         for quote in self:
             quote.quote_approved = True
+
+    @api.onchange('amount_total')
+    def approved_by_discount(self):
+        approved = True
+        for order in self:
+            for line in order.order_line:
+                if line.discount > order.pre_appr_disc and line.discount > order.approved_disc:
+                    approved = False
+                    break
+        for quote in self:
+            quote.quote_approved = approved
+
