3b768abd80b8e3008c0a92810766cdf15ed64c98
pdeoleo <pdeoleo@ipexdr.com>
2020-04-24 16:13:44 -0400

    [CLP] end_ prefix for End Customer fields
    There will be different contact fields, so from now on each one will have a prefix.

--- so_final_consumer/models/purchase.py
+++ so_final_consumer/models/purchase.py
@@ -5,6 +5,7 @@ from odoo import models, fields, api
 class PurchaseOrder(models.Model):
     _inherit = ['purchase.order']
 
-    customer_id = fields.Many2one('res.partner', string='End Customer', tracking=True)
+    end_customer_id = fields.Many2one('res.partner', string='End Customer', tracking=True)
+    end_contact_id = fields.Many2one('res.partner', string='Contact', tracking=True)
+        
     courier_id = fields.Many2one('res.partner', string='Courier', tracking=True)
-    contact_id = fields.Many2one('res.partner', string='Contact', tracking=True)
