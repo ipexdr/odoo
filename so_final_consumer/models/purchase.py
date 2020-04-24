657a38b1d4419558b0b2140018a1c88fa9b55932
pdeoleo <pdeoleo@ipexdr.com>
2020-04-24 16:05:10 -0400

    [ADD] End client contact
    The contact_id many2one field in purchase_views.xml can only be an individual.
    
    Now the client_id can only be a company.

--- so_final_consumer/models/purchase.py
+++ so_final_consumer/models/purchase.py
@@ -5,7 +5,6 @@ from odoo import models, fields, api
 class PurchaseOrder(models.Model):
     _inherit = ['purchase.order']
 
-    client_id = fields.Many2one('res.partner', string='End Customer', tracking=True)
+    customer_id = fields.Many2one('res.partner', string='End Customer', tracking=True)
     courier_id = fields.Many2one('res.partner', string='Courier', tracking=True)
-
-    
+    contact_id = fields.Many2one('res.partner', string='Contact', tracking=True)
