a9885acb4d49c343cfb7aeadaac287ba7a99a7b0
pdeoleo <pdeoleo@ipexdr.com>
2020-04-24 11:14:13 -0400

    [ADD] Courier field
    Added courier_id many2one field. Didn't use res_partner_search_mode in view.

--- so_final_consumer/models/purchase.py
+++ so_final_consumer/models/purchase.py
@@ -6,6 +6,6 @@ class PurchaseOrder(models.Model):
     _inherit = ['purchase.order']
 
     client_id = fields.Many2one('res.partner', string='End Customer', tracking=True)
-    
+    courier_id = fields.Many2one('res.partner', string='Courier', tracking=True)
 
     
