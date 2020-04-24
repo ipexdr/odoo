a8c987a0aa90622d045aa8d5706eff212eb5c5aa
pdeoleo <pdeoleo@ipexdr.com>
2020-04-24 11:08:13 -0400

    [FIX] Removed states from client_id field

--- so_final_consumer/models/purchase.py
+++ so_final_consumer/models/purchase.py
@@ -5,5 +5,5 @@ from odoo import models, fields, api
 class PurchaseOrder(models.Model):
     _inherit = ['purchase.order']
 
-    client_id = fields.Many2one('res.partner', string='Final Client', states=READONLY_STATES, tracking=True)
+    client_id = fields.Many2one('res.partner', string='Final Client', tracking=True)
     
