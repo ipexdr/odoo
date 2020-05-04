c770671408182f45763ee95c143f790893cb721d
pdeoleo <pdeoleo@ipexdr.com>
2020-05-04 15:54:22 -0400

    [FTR][WIP] Second approval level for PO
    Overriding the original button_approve so it is needed for a second person to approve the RFQ.

--- po_end_customer/models/purchase.py
+++ po_end_customer/models/purchase.py
@@ -7,9 +7,15 @@ class PurchaseOrder(models.Model):
 
     end_customer_id = fields.Many2one('res.partner', string='End Customer', tracking=True)
     end_contact_id = fields.Many2one('res.partner', string='Contact', tracking=True)
-    
+
     ref_customer_quote_id = fields.Many2one('sale.order', string='Customer Quote ID', tracking=True)
     
     vendor_contact_id = fields.Many2one('res.partner', string='Vendor Contact', tracking=True)
 
     courier_id = fields.Many2one('res.partner', string='Courier', tracking=True)
+    
+    # Overriding original method to allow two-people approval
+    def button_approve(self, force=False):
+        self.write({'state': 'purchase', 'date_approve': fields.Datetime.now()})
+        self.filtered(lambda p: p.company_id.po_lock == 'lock').write({'state': 'done'})
+        return {}
