c6eef8bd094936fb66cefdfe717c81e784432621
pdeoleo <pdeoleo@ipexdr.com>
2020-05-12 15:03:24 -0400

    [FTR] Select PO Manager for approval requests

--- po_end_customer/models/res_config_settings.py
+++ po_end_customer/models/res_config_settings.py
@@ -1,20 +1,26 @@
 # -*- coding: utf-8 -*-
 
 from odoo import api, fields, models
+from ast import literal_eval
+import logging
+
+_logger = logging.getLogger(__name__)
 
 class ResConfigSettings(models.TransientModel):
     _inherit = 'res.config.settings'
+    po_manager = fields.Many2one('res.users', string="Purchase Orders Manager", help="The user responsible for a PO's final approval.")
     
-    po_manager = fields.Many2one('res.users', string="Purchase Orders Manager", help="The user responsible for a PO's final approval.", default=False)
-
-#     @api.model
-#     def get_values(self):
-#         res = super(ResConfigSettings, self).get_values()
-#         res.update(
-#             po_manager = int(self.env['ir.config_parameter'].sudo().get_param('po_end_customer.po_manager'))
-#         )
-#         return res
-
-#     def set_values(self):
-#         super(ResConfigSettings, self).set_values()
-#         self.env['ir.config_parameter'].sudo().set_param('po_end_customer.po_manager', self.po_manager)
+    def set_values(self):
+    	res = super(ResConfigSettings, self).set_values()
+    	self.env['ir.config_parameter'].sudo().set_param('po_end_customer.po_manager', self.po_manager.id)
+    	return res
+    
+    @api.model
+    def get_values(self):
+        res = super(ResConfigSettings, self).get_values()
+        with_user = self.env['ir.config_parameter'].sudo()
+        po_mgr = with_user.get_param('po_end_customer.po_manager')
+        # _logger.info(f"literal_eval po_mgr -> {literal_eval(po_mgr)}")
+        res.update(
+        	po_manager=(literal_eval(po_mgr)) if po_mgr else False)
+        return res
