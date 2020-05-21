1a6e73c56326988a057a58fe87023bd41c128b90
pdeoleo <pdeoleo@ipexdr.com>
2020-05-21 14:24:45 -0400

    [FTR][WIP] New project_scrum module

---
+++ project_scrum/__manifest__.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "SCRUM for Project",
+
+    'summary': """
+        Adds some scrum-related features to project.""",
+
+    'description': """
+        
+    """,
+
+    'author': "IP Expert DR",
+    'website': "http://www.ipexdr.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Operations/Project',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+
+    'depends': ['project'],
+
+    # always loaded
+    'data': [
+        # 'security/security.xml',
+        # 'security/ir.model.access.csv',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+
+    'css': [
+        'static/src/css/style.css'
+    ]
+}
---
+++ project_scrum/models/project.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+from odoo.exceptions import AccessError, UserError, ValidationError
+import logging
+
+_logger = logging.getLogger(__name__)
+
+class Task(models.Model):
+    _inherit = "project.task"
+
+    iteration = fields.Char('Iteration')
+        
