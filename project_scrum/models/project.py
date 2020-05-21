5b58a2ba03eb463c0da12cb95e17490278fec8ba
pdeoleo <pdeoleo@ipexdr.com>
2020-05-21 17:03:23 -0400

    [FTR][WIP] Project stage

--- project_scrum/models/project.py
+++ project_scrum/models/project.py
@@ -6,6 +6,11 @@ import logging
 
 _logger = logging.getLogger(__name__)
 
+class Project(models.Model):
+    _inherit = 'project.project'
+    stage_id = fields.Many2one('project.task.type', string='Stage', ondelete='restrict', tracking=True, index=True,
+        default=_get_default_stage_id, group_expand='_read_group_stage_ids', copy=False)
+
 class Task(models.Model):
     _inherit = "project.task"
 
