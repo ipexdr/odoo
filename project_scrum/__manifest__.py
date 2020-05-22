da88d37fa289de2c8aca8e0d3dd4cdcb5328fe15
pdeoleo <pdeoleo@ipexdr.com>
2020-05-22 11:44:28 -0400

    [FTR] Working stages for projects
    In addition, there's a new menu for Project Stages.

--- project_scrum/__manifest__.py
+++ project_scrum/__manifest__.py
@@ -16,7 +16,7 @@
     # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Operations/Project',
-    'version': '0.1',
+    'version': '0.5',
 
     # any module necessary for this one to work correctly
 
@@ -27,7 +27,7 @@
         # 'security/security.xml',
         'security/ir.model.access.csv',
         'views/project_views.xml',
-#         'data/project_stages.xml',
+        'data/project_stages.xml',
 #         'security/ir.model.access.csv'
     ],
     # only loaded in demonstration mode
--- project_scrum/models/project.py
+++ project_scrum/models/project.py
@@ -10,14 +10,23 @@ _logger = logging.getLogger(__name__)
 class ProjectStage(models.Model):
     _name = 'project.stage'
     _description = 'Project Stage'
-    _order = 'id'
-
+    _order = 'sequence, id'
+    
+    sequence = fields.Integer(default=1)
     name = fields.Char('Stage Name')
+    fold = fields.Boolean('Folded', default=False)
+    description = fields.Text()
 
 class Project(models.Model):
     _inherit = 'project.project'
-    stage_id = fields.Many2one('project.stage', string='Stage', ondelete='restrict', tracking=True, index=True,
-    copy=False)
+    
+    @api.model
+    def _read_group_stage_ids(self, stages, domain, order):
+        stage_ids = self.env['project.stage'].search([])
+        return stage_ids
+    
+    stage_id = fields.Many2one('project.stage', tracking=True, string='Stage', ondelete='restrict', index=True,
+    copy=False, group_expand='_read_group_stage_ids')
 
 
 class Task(models.Model):
