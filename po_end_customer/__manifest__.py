bcf82aa9cfc0c58516d075fc75f8c8ca3293fb6e
pdeoleo <pdeoleo@ipexdr.com>
2020-06-01 12:54:59 -0400

    Versioning

--- po_end_customer/__manifest__.py
+++ po_end_customer/__manifest__.py
@@ -30,7 +30,7 @@
     # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Purchase',
-    'version': '1.0',
+    'version': '1.1',
 
     # any module necessary for this one to work correctly
     'depends': ['purchase', 'sale', 'po_approval'],
