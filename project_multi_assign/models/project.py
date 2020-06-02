fd25da9791b112246751ba29e7f2a9b31719f48c
pdeoleo <pdeoleo@ipexdr.com>
2020-06-02 09:34:25 -0400

    [FTR] Assigned user not visible in support ids field
    When user_id changes, support_user_ids domain is modified to hide user_id from available users.

--- project_multi_assign/models/project.py
+++ project_multi_assign/models/project.py
@@ -10,3 +10,13 @@ class Task(models.Model):
     _inherit = "project.task"
 
     support_user_ids = fields.Many2many('res.users', string="Support Contact")
+    
+    @api.onchange('user_id')
+    def _onchange_support_user_ids(self):
+        """
+        When user_id changes, support_user_ids domain will change to avoid
+        selected user_id to appear in many2many field
+        """
+        if self.user_id:
+            domain = [('id', '!=', self.user_id.id)]
+            return {'domain': {'support_user_ids': domain}}
