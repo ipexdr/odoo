9c274054a0fbdb6487968291e09b00303faf6ddf
pdeoleo <pdeoleo@ipexdr.com>
2020-06-03 10:44:54 -0400

    [FTR] Only new support users wil be notified
    If a user is not new after editing Task, they wont be notified.

--- project_multi_assign/models/project.py
+++ project_multi_assign/models/project.py
@@ -25,35 +25,65 @@ class Task(models.Model):
         else:
             return {'domain':{'support_user_ids':[]}}
         
-    # @api.onchange('support_user_ids')
-    # def _subscribe_support_user_ids(self):
-    #     """
-    #     When 
-    #     """
-    #     partner_ids = [user.partner_id.id for user in self._origin.support_user_ids]
-    #     self._origin.message_subscribe(partner_ids)
-        
-    def notify_support_users(self):
+    def notify_support_users(self, users=None):
         """
         Takes the many2many field partner_ids and notifies them about
         the task assignation.
         """
-        support_partners = set([user.partner_id.id for user in self.support_user_ids])
-        message = f"You have been assigned as a support contact to the task {self.name}."
-        _logger.info(support_partners)
-        self.message_post(
-            subject=f"You have been assigned as support to {self.name}",
-            body=message,
-            partner_ids = support_partners
-        )
+
+        # If there are no specified users, every user in the support_user_ids
+        # field will be notified
+        # (This happens only in create() method)
+        if users is None:
+            support_partners = set([user.partner_id.id for user in self.support_user_ids])
+            names = [user.partner_id.name for user in self.support_user_ids] 
+        else:
+            support_partners = set([user.partner_id.id for user in users])
+            names = [user.partner_id.name for user in users] 
+            
+        # According to the number of members assigned, the message will be different
+        if len(support_partners) > 0:
+            # Names separated by comma if > 1
+            msg_names = ', '.join(names) if len(support_partners) > 1 else names[0]
+
+            if len(support_partners) > 1:
+                message = f"{msg_names} have been assigned as a support contact to the task {self.name}."
+            else:
+                message = f"{msg_names} has been assigned as a support contact to the task {self.name}."
+            
+            _logger.info(support_partners)
+            self.message_post(
+                subject=f"You have been assigned as support to {self.name}",
+                body=message,
+                partner_ids = support_partners
+            )
     
     def write(self, vals):
-        self.notify_support_users()
-            
-        super(Task, self).write(vals)
-        return True
+        # Storing old data for comparison
+        pre_ids = self.support_user_ids
         
-    # TODO: Overwrite create() to allow notification when record is created
+        # Writing database changes
+        res = super(Task, self).write(vals)
+        
+        # Taking new data
+        post_ids = self.support_user_ids
     
-    # TODO: Distinguish if user has been added or not when notifying the assignation,
-    # instead of notifying everyone
+        # If there is a difference between old and
+        # new data, the loop will check if there are
+        # new users to be notified
+        if pre_ids != post_ids:
+            new_users = set()
+            
+            for user in post_ids:
+                if user not in pre_ids:
+                    new_users.add(user)
+            self.notify_support_users(users=new_users)
+            
+        return res
+        
+    @api.model
+    def create(self, vals):
+        task = super(Task, self).create(vals)
+        if vals.get('support_user_ids'):
+            task.notify_support_users()
+        return task
