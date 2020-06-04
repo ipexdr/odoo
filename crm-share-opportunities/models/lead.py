ccae70e4dae04b9a0d638f3370e2885e5fe87e53
pdeoleo <pdeoleo@ipexdr.com>
2020-06-04 16:20:19 -0400

    [FTR] Only salesmen visible in Shared Users
    Note: Logged user and user_id are visible in field.

--- crm-share-opportunities/models/lead.py
+++ crm-share-opportunities/models/lead.py
@@ -9,6 +9,10 @@ _logger = logging.getLogger(__name__)
 class Lead(models.Model):
     _inherit = "crm.lead"
     
-    # The field wont show the user currently logged in - if the user can already see the opportunity, there's no 
-    # need to add himself.
-    shared_users_id = fields.Many2many('res.users', string="Shared Contacts", domain="[('id', '!=', uid)]")
+    # The field will only show salesmen
+    shared_users_id = fields.Many2many(
+        'res.users', 
+        string="Share with Users", 
+        domain=lambda self: [("groups_id", "in", self.env.ref("sales_team.group_sale_salesman").id)])
+
+    # TODO: Assigned salesman (user_id) must not appear in field
