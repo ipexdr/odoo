d5a0f76f24aa586764d6a93a60e5194c754217e3
pdeoleo <pdeoleo@ipexdr.com>
2020-10-21 18:05:43 -0400

    Onchange and oncreate methods
    Takes next sequence value withou affecting seq. counter, just in case the field is later modified.
    
    Affects the counter only when creating the move. Still pending to make it work when writing on existing move.

--- accounting/ncf_generator/models/account_move.py
+++ accounting/ncf_generator/models/account_move.py
@@ -8,17 +8,41 @@ _logger = logging.getLogger(__name__)
 class AccountMove(models.Model):
     _inherit = ['account.move']
     
-    NFC_TYPES = [
-        ('gasto_menor', 'Gasto Menor'),
-        ('prov_informal', 'Proveedor Informal'),
-        ('cons_final', 'Consumidor Final'),
-        ('gubernamental', 'Gubernamental'),
-        ('reg_especal', 'Regimen Especial'),
-        ('credito_fiscal', 'Credito Fiscal'),
-        ('nota_credito', 'Nota Credito'),
-        ('nota_debito', 'Nota Debito')
+    NCF_TYPES = [
+        ('ncf.gasto.menor', 'Gasto Menor'),
+        ('ncf.prov.informal', 'Proveedor Informal'),
+        ('ncf.con.final', 'Consumidor Final'),
+        ('ncf.gubernamental', 'Gubernamental'),
+        ('ncf.reg.especial', 'Regimen Especial'),
+        ('ncf.credito.fiscal', 'Credito Fiscal'),
+        ('ncf.nota.credito', 'Nota Credito'),
+        ('ncf.nota.debito', 'Nota Debito')
     ]
     
     ncf = fields.Char('NCF')
-    ncf_type = fields.Selection(NFC_TYPES, string='NCF Type')
+    ncf_type = fields.Selection(NCF_TYPES, string='NCF Type')
     
+    @api.onchange('ncf_type')
+    def change_ncf(self):
+        for move in self:
+            if move.ncf_type:
+                # Takes next sequence value without affecting the sequence counter
+                sequence = self.env['ir.sequence'].search([('code','=',move.ncf_type)])
+                next = sequence.get_next_char(sequence.number_next_actual) or ''
+                move.write({'ncf': next})
+            else:
+                move.write({'ncf': ''})
+
+    # on create method
+    @api.model
+    def create(self, vals):
+        move = super(AccountMove, self).create(vals)
+        if move.ncf_type and move.ncf:
+            sequence = self.env['ir.sequence'].search([('code','=',move.ncf_type)])
+            # Applies the sequence value and adds to the sequence counter
+            if move.ncf == sequence.get_next_char(sequence.number_next_actual):
+                self.env['ir.sequence'].get(move.ncf_type)
+            
+        return move
+
+# TODO: On write method
