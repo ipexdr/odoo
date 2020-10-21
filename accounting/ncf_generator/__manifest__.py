bd3403a00d0a4a40c5c2cd2d31a9883772e57ff3
pdeoleo <pdeoleo@ipexdr.com>
2020-10-21 15:14:38 -0400

    new module NCF Generator

---
+++ accounting/ncf_generator/__manifest__.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "NCF Generator",
+
+    'summary': """
+        Allows categorization and automatic generation of NCF for invoices.""",
+
+    'description': """
+        
+    """,
+
+    'author': "IP Expert DR",
+    'website': "http://www.ipexdr.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Accounting',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctlyy
+
+    'depends': ['account'],
+
+    # always loaded
+    'data': [
+        # 'security/security.xml',
+        # 'security/ir.model.access.csv',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+
+    'css': [
+        'static/src/css/style.css'
+    ]
+}
