2aeb7e211a43efc5734a27efd55cd87db36b8bda
pdeoleo <pdeoleo@ipexdr.com>
2020-10-27 14:55:39 -0400

    [ADD] auto ncf assignation test
    Testing if ncf auto assignation works one after the other.

--- accounting/ncf_generator/tests/test_account_move_ncf.py
+++ accounting/ncf_generator/tests/test_account_move_ncf.py
@@ -20,11 +20,35 @@ class TestAccountMoveNCF(TransactionCase):
     def test_ncf_char_max_length(self):
         '''Ensure the length of the NCF is equals to 11 even if it is given more characters, after been saved'''
         form = Form(self.account_move)
-        
         form.ncf = 'XXXXXXXXXXXXX'
         form.save()
         self.assertEqual(len(form.ncf), 11)
-    
+        
+    def test_auto_ncf_save_sequence(self):
+        '''Ensure that NCF is saved and next one is generated correctly afterwards (when assigned automatically)
+        -Assuming ncf.gasto.menor sequence parameters:
+        --Prefix	      B13
+        --Sequence Size	  8
+        --Step	          1
+        --Next Number	  3
+        '''
+        
+        form1 = Form(self.account_move)
+        form1.ncf_type = 'ncf.gasto.menor'    
+        form1.save()
+        self.assertEqual(form1.ncf, 'B1300000001')
+        
+        form2 = Form(self.account_move)
+        form2.ncf_type = 'ncf.gasto.menor'
+        form2.save()
+        self.assertEqual(form2.ncf, 'B1300000002')
+        
+        form3 = Form(self.account_move)
+        form3.ncf_type = 'ncf.gasto.menor'
+        form3.save()
+        self.assertEqual(form3.ncf, 'B1300000003')
+        
+        
     # TODO: Test when move is saved/created
     
     # TODO: ??? Test when input used NCF
