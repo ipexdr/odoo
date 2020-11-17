e5d2f795007dcc947c6d8aa62953a33d6d452ce0
pdeoleo <pdeoleo@ipexdr.com>
2020-11-17 16:02:57 -0400

    [FIX] view route

--- accounting/ncf_generator/__manifest__.py
+++ accounting/ncf_generator/__manifest__.py
@@ -16,7 +16,7 @@
     # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Accounting',
-    'version': '0.10.3',
+    'version': '0.10.4',
 
     # any module necessary for this one to work correctlyy
 
@@ -29,7 +29,7 @@
         'security/ir.model.access.csv',
         'views/ncf_sequence_views.xml',
         'views/account_move_views.xml',
-        'web_external_layout.xml',
+        'views/web_external_layout.xml',
         'views/report_invoice.xml',
     ],
     # only loaded in demonstration mode
