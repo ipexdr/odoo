8ffa4cf06e7a024de98aa5a31ba7300b7f09e004
pdeoleo <pdeoleo@ipexdr.com>
2020-11-25 10:20:51 -0400

    Manifest changes
    Removed depends. Changed version to 0.1. Changed module name and description.

--- sales/ipx_sales_approval/__manifest__.py
+++ sales/ipx_sales_approval/__manifest__.py
@@ -1,14 +1,13 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "Sale Order Approval process",
+    'name': "Sales Approval Process",
 
     'summary': """
-        Allows for Sales Order approval process""",
+        Adds an approval process for Sales Orders""",
 
     'description': """
-        Based on Purchase's approval process, adds the capability of
-        asking for a manager's approval in case some fields are out 
-        of their predefined values.
+        Adds an approval process if some fields are out
+        of a predefined range.
     """,
 
     'author': "IP Expert DR",
@@ -18,11 +17,11 @@
     # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Sales',
-    'version': '1.2',
+    'version': '0.1',
 
     # any module necessary for this one to work correctly
 
-    'depends': ['sale_management', 'ipx_quote_fields', 'log_wizard'],
+    'depends': ['sale_management', 'log_wizard'],
 
     # always loaded
     'data': [
