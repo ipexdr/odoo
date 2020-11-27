a9b9a2e329f15bf85a543b60961f719b191af4a5
pdeoleo <pdeoleo@ipexdr.com>
2020-11-27 16:46:37 -0400

    [WIP] approval asking adjustments

--- sales/ipx_sales_approval/models/sale_order.py
+++ sales/ipx_sales_approval/models/sale_order.py
@@ -86,63 +86,37 @@ class SaleOrder(models.Model):
     def action_ask_approval(self):
         all_users = self.env['res.users'].search([('active', '=', True)])
 
-        # TODO: Specify managers depending on extra vendor discount
-
-        if self.min_margin <= self.var_lvl_2_margin:
+        if self.approve_level == 2:
             my_users_group = all_users.filtered(lambda user: user.has_group(
                 'ipx_sales_approval.ipx_sales_approval_manager'))
         else:
             my_users_group = all_users.filtered(lambda user: user.has_group(
                 'ipx_sales_approval.ipx_sales_approval_assistant'))
+        
         so_number = self.name
-
         order_id = self.id
 
         domain = self.env['ir.config_parameter'].sudo(
         ).get_param('web.base.url')
-
         url = f"{domain}/web#id={order_id}&action=334&model=sale.order&view_type=form&cids=1&menu_id=192"
 
         msg = f"<p>The Quotation {so_number} needs to be approved.</p>"
 
-        for order in self:
-            order.quote_margin_approved = self.approved_by_margin(order)
-            order.quote_vendor_discount_approved = self.approved_by_extra_discount(
-                order)
+        exceeded_items = []
 
-        if not self.quote_margin_approved:
-            exceeded_items = []
+        # Knowing which items are the reason for the approval
+        
+        for line in self.order_line:
+            if not line.is_approved:
+                exceeded_items.append(
+                    {'item': line.product_id.name, 'margin': (line.real_margin * 100)})
 
-            # Knowing which items are the reason for the approval
-            for order in self:
-                for line in order.order_line:
-                    if line.real_margin <= line.min_appr_margin:
-                        exceeded_items.append(
-                            {'item': line.product_id.name, 'margin': (line.real_margin * 100)})
+        msg += "<p>Items over the discount limit:</p><ul>"
 
-            msg += "<p>Items over the discount limit:</p><ul>"
+        for item in exceeded_items:
+            msg += f"<li>\t{item['item']} - Profit Margin: {round(item['margin'], 2)}%</li>"
 
-            for item in exceeded_items:
-                msg += f"<li>\t{item['item']} - Profit Margin: {round(item['margin'], 2)}%</li>"
-
-            msg += "</ul>"
-
-        if not self.quote_vendor_discount_approved:
-            exceeded_items = []
-
-            # Knowing which items are the reason for the approval
-            for order in self:
-                for line in order.order_line:
-                    if line.extra_discount < line.vendor_discount or line.extra_discount > line.approved_extra_discount:
-                        exceeded_items.append(
-                            {'item': line.product_id.name, 'vendor_discount': (line.extra_discount * 100)})
-
-            msg += "<p>Items with unapproved vendor discounts:</p><ul>"
-
-            for item in exceeded_items:
-                msg += f"<li>\t{item['item']} - Vendor Discount: {round(item['vendor_discount'], 2)}%</li>"
-
-            msg += "</ul>"
+        msg += "</ul>"
 
         partner_ids = []
         for user in my_users_group:
