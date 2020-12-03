b7c675976282d5a35fc16232a9b7ad3bd8de35d7
pdeoleo <pdeoleo@ipexdr.com>
2020-12-03 17:40:29 -0400

    [ADD] Show manufacturer regardless of delivery state

--- product/ipx_product_manufacturer/__manifest__.py
+++ product/ipx_product_manufacturer/__manifest__.py
@@ -16,7 +16,7 @@
     # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Sales',
-    'version': '0.0.1',
+    'version': '0.1.0',
 
     # any module necessary for this one to work correctly
 
