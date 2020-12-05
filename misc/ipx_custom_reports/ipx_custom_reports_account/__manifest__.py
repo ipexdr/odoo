3ef9412e652d80cc0fc97230ffa4fd3387474005
pdeoleo <pdeoleo@ipexdr.com>
2020-12-05 15:11:36 -0400

    [ADD] account: invoice salesperson info
    Removed default partner_id address and replaced for unordered list
    
    Default address is forced to right column.
    Added salesperson info in left column.

--- misc/ipx_custom_reports/ipx_custom_reports_account/__manifest__.py
+++ misc/ipx_custom_reports/ipx_custom_reports_account/__manifest__.py
@@ -16,7 +16,7 @@
     # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Accounting',
-    'version': '0.1',
+    'version': '0.2',
 
     # any module necessary for this one to work correctly
 
@@ -26,7 +26,7 @@
     'data': [
         # 'security/security.xml',
         # 'security/ir.model.access.csv',
-        # 'wizard/log_message_wizard_view.xml'
+        'view/report_invoice.xml'
     ],
     # only loaded in demonstration mode
 }
