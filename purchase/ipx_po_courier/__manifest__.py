84b7ae993188b50a2d292b28dab19e79b7db1412
pdeoleo <pdeoleo@ipexdr.com>
2020-12-10 12:44:36 -0400

    [ADD] ipx_po_courier: first module commit
    Takes po courier info way from ipx_end_customer

---
+++ purchase/ipx_po_courier/__manifest__.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "End Customer in PO",
+
+    'summary': """
+        End customer field in PO""",
+
+    'description': """
+        IP Expert module that adds relevant info about
+        the PO's end client.
+        
+        - Name
+        - Address (only in Report)
+        - Contact (only in Report)
+        - Phone (only in Report)
+        - Website (only in Report)
+        - Contact Email (only in Report)
+    """,
+
+    'author': "IP Expert DR",
+    'website': "http://www.ipexdr.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Purchase',
+    'version': '1.2.0',
+
+    # any module necessary for this one to work correctly
+    'depends': ['purchase', 'paperformat_us_landscape'],
+
+    # always loaded
+    'data': [
+        # 'security/security.xml',
+        # 'security/ir.model.access.csv',
+        'views/purchase_views.xml',
+        'reports/po_report.xml',
+        'reports/rfq_report.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+
+    'css': [
+        'static/src/css/style.css'
+    ]
+}
