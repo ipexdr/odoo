4749f745c4be5c2163e5c55f6b2130cc0aacfdc5
pdeoleo <pdeoleo@ipexdr.com>
2020-12-10 12:45:49 -0400

    [REF] ipx_po_end_customer: removed anything not directly related to end customer
    Removed appproval related code. All that will be moved to another add-on module.
    
    Removed courier related code. Moved to ipx_po_courier.

--- purchase/ipx_po_end_customer/__manifest__.py
+++ purchase/ipx_po_end_customer/__manifest__.py
@@ -1,26 +1,17 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "End Customer and Courier in PO",
+    'name': "Courier in Purchase Orders",
 
     'summary': """
-        Adds the end customer and courier fields to the PO model""",
+        Adds courier field to the Purchase Orders""",
 
     'description': """
         IP Expert module that adds relevant info about
-        the PO's end client.
-        
-        End customer fields:
-        - Name
-        - Address (only in PDF)
-        - Contact (only in PDF)
-        - Phone (only in PDF)
-        - Website (only in PDF)
-        - Contact Email (only in PDF)
+        the Courier.
 
-        Courier fields:
         - Name
-        - Address (only in PDF)
-        - Phone (only in PDF)
+        - Address (only in Report)
+        - Phone (only in Report)
     """,
 
     'author': "IP Expert DR",
@@ -30,26 +21,15 @@
     # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Purchase',
-    'version': '1.1.1',
+    'version': '0.0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['purchase', 'sale', 'ipx_po_approval'],
+    'depends': ['purchase', 'paperformat_us_landscape'],
 
     # always loaded
     'data': [
-        # 'security/security.xml',
-        # 'security/ir.model.access.csv',
         'views/purchase_views.xml',
         'reports/po_report.xml',
         'reports/rfq_report.xml',
-        'data/paperformat_us_landscape.xml'
-    ],
-    # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
-    ],
-
-    'css': [
-        'static/src/css/style.css'
     ]
 }
--- purchase/ipx_po_end_customer/models/purchase.py
+++ purchase/ipx_po_end_customer/models/purchase.py
@@ -24,55 +24,5 @@ class PurchaseOrder(models.Model):
 
     courier_id = fields.Many2one('res.partner', string='Courier', tracking=True)
 
-    is_vendor_quote = fields.Boolean('Vendor Quote is attached', store=True, default=False)
-    is_customer_po = fields.Boolean('Customer PO is attached', store=True, default=False)
-    
-    # module_po_approval_installed = fields.Boolean(
-    #                        compute='_compute_module_po_approval_installed',
-    #                        string='Is PO Approval installed?')
-
-    # @api.depends()
-    # def _compute_module_po_approval_installed(self):
-    #     po_approval_installed = self.env['ir.module.module'].search([('name', '=', 'po_approval')])
-    #     module_po_approval_installed = True if module and module.state == 'installed' else False
-    #     for record in self:
-    #          # no need for update when you set only one field just use normal asignement 
-    #         record.module_po_approval_installed = module_po_approval_installed
-    
-    # Overriding original method to verify if required files
-    # are uploaded
-    def button_confirm(self):
-        for order in self:
-            if order.is_vendor_quote and order.is_customer_po:
-                if order.state not in ['draft', 'sent']:
-                    continue
-                order._add_supplier_to_product()
-                # Deal with double validation process
-                if order.company_id.po_double_validation == 'one_step'\
-                        or (order.company_id.po_double_validation == 'two_step'\
-                            and order.amount_total < self.env.company.currency_id._convert(
-                                order.company_id.po_double_validation_amount, order.currency_id, order.company_id, order.date_order or fields.Date.today()))\
-                        or order.user_has_groups('purchase.group_purchase_manager'):
-                    order.button_approve(override=True)
-                else:
-#                     if order.module_po_approval_installed:
-                    all_users = self.env['res.users'].search([('active', '=', True)])
-
-                    managers = all_users.filtered(lambda user: user.has_group('purchase.group_purchase_manager'))
-                    assistants = all_users.filtered(lambda user: user.has_group('po_approval.group_purchase_assistant'))
-
-                    partner_ids = []
-                    for user in assistants:
-                        if user not in managers:
-                            partner_ids.append(user.partner_id.id)
-
-                    self.message_post(
-                        subject="PO Waiting for Pre-Approval",
-                        body=f"The PO {self.name} needs a review for pre-approval.",
-                        partner_ids=partner_ids
-                    )
-                    
-                    order.write({'state': 'to approve'})
-            else:
-                raise UserError("Unable to confirm order. Please check that the required files are attached.")
-        return True
+    # is_vendor_quote = fields.Boolean('Vendor Quote is attached', store=True, default=False)
+    # is_customer_po = fields.Boolean('Customer PO is attached', store=True, default=False)
