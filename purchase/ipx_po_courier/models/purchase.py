5abd919531dcdaa45a01f6916f3241ac1f3e13fd
pdeoleo <pdeoleo@ipexdr.com>
2020-12-10 12:44:36 -0400

    [ADD] ipx_po_courier: first module commit
    Takes po courier info way from ipx_end_customer

---
+++ purchase/ipx_po_courier/models/purchase.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+from odoo.exceptions import AccessError, UserError, ValidationError
+import logging
+
+_logger = logging.getLogger(__name__)
+
+class PurchaseOrder(models.Model):
+    _inherit = ['purchase.order']
+
+    READONLY_STATES = {
+        'purchase': [('readonly', True)],
+        'done': [('readonly', True)],
+        'cancel': [('readonly', True)],
+    }
+
+    courier_id = fields.Many2one('res.partner', string='Courier', tracking=True)
