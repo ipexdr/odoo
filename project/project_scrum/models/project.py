a12d76535f0aa2e3d7103c7bcd6c8a092e993265
pdeoleo <pdeoleo@ipexdr.com>
2021-02-17 13:43:10 -0400

    [IMP] Heavy iteration additions
    - project.iteration name and end_date are computed automatically
    
    - project.iteration.template when create new iterations are created. Display name is also computed

--- project/project_scrum/models/project.py
+++ project/project_scrum/models/project.py
@@ -2,6 +2,8 @@
 
 from odoo import models, fields, api
 from odoo.exceptions import AccessError, UserError, ValidationError
+from dateutil.relativedelta import relativedelta
+from datetime import datetime
 import logging
 
 _logger = logging.getLogger(__name__)
@@ -39,10 +41,27 @@ class Iteration(models.Model):
     _name = 'project.iteration'
     _description = 'Iteration'
     
-    start_date = fields.Date("Start Date", default=fields.Date.context_today)
-    end_date = fields.Date("End Date", default=fields.Date.context_today)
+    def name_get(self):
+        res = []
+        for iteration in self:
+            res.append((iteration.id, iteration.display_name))
+        return res
     
-    iteration_template_id = fields.Many2one('project.iteration.template', string='Iteration Template')
+    @api.depends('iteration_template_id', 'start_date')
+    def _compute_display_name(self):
+        for iteration in self:
+            iteration.display_name = f"{iteration.start_date} | {iteration.end_date}"
+
+    @api.depends('iteration_template_id', 'start_date')
+    def _compute_end_date(self):
+        for iteration in self:
+            iteration_template_days = iteration.iteration_template_id.iteration_length
+            iteration.end_date = iteration.start_date + relativedelta(days = iteration_template_days)
+    
+    start_date = fields.Date("Start Date", default=fields.Date.context_today, required=True)
+    end_date = fields.Date("End Date", default=fields.Date.context_today, compute='_compute_end_date')
+    
+    iteration_template_id = fields.Many2one('project.iteration.template', string='Iteration Template', required=True)
     
     display_name = fields.Char('Iteration name')
 
@@ -50,12 +69,31 @@ class IterationTemplate(models.Model):
     _name = 'project.iteration.template'
     _description = 'Iteration Template'
     
+    @api.model
+    def create(self, values):
+        new_date = values['first_iteration_date']
+        res = super(IterationTemplate, self).create(values)
+        for i in range(4):
+            new_vals = {
+                'start_date':new_date,
+                'iteration_template_id':res.id
+            }
+            self.env['project.iteration'].create(new_vals)
+            new_date = res.first_iteration_date + relativedelta(days = res.iteration_length)
+            
+        return res
+    
     def name_get(self):
         res = []
         for template in self:
             res.append((template.id, template.display_name))
         return res
+
+    @api.depends('iteration_length')
+    def _compute_display_name(self):
+        for template in self:
+            template.display_name = f"{template.iteration_length}-Day iteration"
     
-    iteration_length = fields.Integer('Iteration Length (days)', default=5)
-    first_iteration_date = fields.Date('First Iteration')
-    display_name = fields.Char('Iteration Template Name')
+    iteration_length = fields.Integer('Iteration Length (days)', default=5, required=True)
+    first_iteration_date = fields.Date('First Iteration', required=True)
+    display_name = fields.Char('Iteration Template Name', compute='_compute_display_name')
