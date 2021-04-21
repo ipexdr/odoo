80d8cdb41b48c557af1d11cdc6d868470139cdb1
pdeoleo <pdeoleo@ipexdr.com>
2021-04-21 15:19:12 -0700

    First ipx_retention_letter commit
    Basic QWeb view creation and module scaffolding.

---
+++ accounting/ipx_retention_letter/__manifest__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Retention Letter",
+
+    'summary': """
+        Adds Retention Letter action when a Retention-type tax is selected in an invoice.""",
+
+    'description': """
+        
+    """,
+
+    'author': "IP Expert DR",
+    'website': "http://www.ipexdr.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Accounting',
+    'version': '0.0.1',
+
+    # any module necessary for this one to work correctlyy
+
+    'depends': ['account'],
+
+    # always loaded
+    'data': [
+        'views/report_retention_letter.xml'
+        
+    ]
+}
